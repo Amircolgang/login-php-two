a83ef43bd84d52e6ed872ad07e3834ab799f6fd2
amircolgang <parham38385747@gmail.com>
2025-08-13 04:46:44 -0700

    Amircolgang: changes on 2025-08-13 04:46:44

--- action/sing-up.php
+++ action/sing-up.php
@@ -6,6 +6,6 @@ if(isset($_POST["signup"])){
         $username = $_POST["username"];
         $email = $_POST["email"] ;
         $password = $_POST["password"];
-        $mobile = $_POST["m"]
+        $mobile = $_POST["mobile"]
 
 };

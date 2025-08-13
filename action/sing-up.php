765e8607f9dfa2bc3ecda0b26c6b9bbe9c3e33d6
amircolgang <parham38385747@gmail.com>
2025-08-13 04:45:40 -0700

    Amircolgang: changes on 2025-08-13 04:45:40

--- action/sing-up.php
+++ action/sing-up.php
@@ -3,5 +3,9 @@
 require_once("../config/loader.php");
 
 if(isset($_POST["signup"])){
-        $username = $_POST["username"]
+        $username = $_POST["username"];
+        $email = $_POST["email"] ;
+        $password = $_POST["password"];
+        $mobile = $_POST["m"]
+
 };

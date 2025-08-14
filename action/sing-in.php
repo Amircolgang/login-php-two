50b524e25dd636093aa1370d29ce35bae12ae2c0
amircolgang <parham38385747@gmail.com>
2025-08-14 05:54:19 -0700

    Amircolgang: changes on 2025-08-14 05:54:18

--- action/sing-in.php
+++ action/sing-in.php
@@ -8,13 +8,8 @@ if(isset($_POST["signin"])){
         $password =$_POST["password"]; // هش کردن رمز عبور
        
         
-        // بررسی مقادیر ضروری
       
-        // بررسی فرمت ایمیل
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            throw new Exception("فرمت ایمیل نامعتبر است");
-        }
-
+    
         // آماده‌سازی کوئری
         $query = "SELECT users WHERE (username = :key OR mobile = :key OR email = :key ) AND (password = :password)";
         
